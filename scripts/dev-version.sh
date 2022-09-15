389ef8e06295bdbf6e424006e0c63dfb1811d412
rzadp <roopert7@gmail.com>
2022-09-15 14:52:27 +0200

    Add a Release script for dev versions

---
+++ scripts/dev-version.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Will append a "-dev.commithash" suffix to version,
+# and change NPM release channel to "dev".
+# Used to auto-publish dev version from main branch .
+
+set -e
+DEV=$(jq -r ".version" package.json | awk -F "-" '{print $1}')-dev.$(git rev-parse --short HEAD)
+cat <<< "$(jq --arg DEV "$DEV" ".version = \"$DEV\"" package.json)" > package.json
+cat <<< "$(jq ".publishConfig.tag = \"dev\"" package.json)" > package.json
